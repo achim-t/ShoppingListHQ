b1818833ba676ee58723bbfe1f2de738016a50ba
Achim Taeschner <ataeschner@LeBit.com>
2016-04-28 10:23:26 +0200

    Init

---
+++ app/src/main/java/de/programierenlernenhq/shoppinglisthq/MainActivity.java
@@ -0,0 +1,34 @@
+package de.programierenlernenhq.shoppinglisthq;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by ataeschner on 28.04.2016.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
